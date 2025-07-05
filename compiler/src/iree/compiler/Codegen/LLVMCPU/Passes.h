7861c58e0d9a5213fe525861e5ce54a1686570bb
Ege Beysel <beyselege@gmail.com>
2025-07-05 10:45:21 +0000

    [DT][SVE] interplay between DT & tiling for mmt4d - first draft
    Signed-off-by: Ege Beysel <beyselege@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.h
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.h
@@ -79,6 +79,7 @@ struct LLVMCPUPipelineOptions {
   bool useConfiguredVectorSizes = true;
   bool enablePeeling = false;
   bool enableVectorMasking = false;
+  bool enableAArch64SVE = false;
   bool enableAArch64SME = false;
   bool enableAArch64I8mm = false;
   bool lowerToAVX2 = false;
