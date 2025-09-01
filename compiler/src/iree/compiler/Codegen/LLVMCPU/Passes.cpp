5fdee4f82fe904915e0f6c82f519b6a6d2a51ff9
Ege Beysel <beyselege@gmail.com>
2025-09-01 15:58:01 +0000

    [DT][SVE]: temporary fixup: let multi tiling expert pipeline use last op for tile and fuse

--- compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMCPU/Passes.cpp
@@ -238,7 +238,7 @@ void addMultiTilingExpertPassPipeline(
     case IREE::CPU::TilingLevel::CacheParallelTiles:
     case IREE::CPU::TilingLevel::VectorCommonParallelTiles:
       funcPassManager.addPass(
-          createLLVMCPUTileAndFuseProducerConsumerPass(level));
+          createLLVMCPUTileLastOpAndFuseProducerConsumerPass(level));
       break;
     case IREE::CPU::TilingLevel::CacheReductionTiles:
       funcPassManager.addPass(
