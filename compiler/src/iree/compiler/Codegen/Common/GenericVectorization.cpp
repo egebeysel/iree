1589285dd128ccdd02301db51b35e3528d523087
Ege Beysel <beyselege@gmail.com>
2025-09-08 13:31:42 +0000

    [DT][SVE] add vector transfer_read/write legalizer and enable bf16 & i8mm lowerings
    Signed-off-by: Ege Beysel <beyselege@gmail.com>

--- compiler/src/iree/compiler/Codegen/Common/GenericVectorization.cpp
+++ compiler/src/iree/compiler/Codegen/Common/GenericVectorization.cpp
@@ -198,8 +198,13 @@ void GenericVectorizationPass::runOnOperation() {
       (void)IREE::VectorExt::vectorizeLinalgExtGatherToTransferGather(
           rewriter, gatherOp, vectorSizes);
     } else {
-      FailureOr<linalg::VectorizationResult> result = linalg::vectorize(
-          rewriter, op, vectorSizes, scalableVecDims, vectorizeGatherAccesses);
+      FailureOr<linalg::VectorizationResult> result =
+          linalg::vectorize(rewriter, op, vectorSizes, scalableVecDims,
+                            vectorizeGatherAccesses, false,
+                            /* assumeScalableSizesMultipleOfDim */
+                            isa<linalg::Mmt4DOp, linalg::BatchMmt4DOp>(op),
+                            /* createNamedContraction */
+                            isa<linalg::Mmt4DOp, linalg::BatchMmt4DOp>(op));
       if (succeeded(result)) {
         rewriter.replaceOp(op, result->replacements);
       }
